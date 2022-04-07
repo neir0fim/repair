52850f775c56544eafa2b714ffa79c21887cec8f
neir0fim <kuzinsergej222@gmail.com>
2022-04-07 20:37:03 +0300

    Start commit

---
+++ src/test/java/com/kuzin/repair/RepairApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kuzin.repair;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepairApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
