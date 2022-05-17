575ad19116e380d2c1828c3125f84817eb82eb25
neir0fim <kuzinsergej222@gmail.com>
2022-05-17 17:00:14 +0300

    continue developing project

--- web-app/src/main/java/com/kuzin/web/mvc/MvcController.java
+++ web-app/src/main/java/com/kuzin/web/mvc/MvcController.java
@@ -13,8 +13,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 /** mvc controller class.*/
 @Controller
 public class MvcController {
-
-
     UnitService service;
     ArticleService articleService;
 
